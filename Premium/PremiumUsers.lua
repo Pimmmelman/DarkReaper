10cb1b36d23cd035b26208bd7195d94a111299aa
Kxyz <75092791+xXKxyzXx@users.noreply.github.com>
2022-02-08 16:26:12 +0100

    Update PremiumUsers.lua

--- Premium/PremiumUsers.lua
+++ Premium/PremiumUsers.lua
@@ -1,1 +1,98 @@
+local Diamond = {
 
+}
+
+local cucumber = {
+
+}
+
+local Raider = {
+
+}
+
+local ABC = {
+
+}
+
+local iscool = {
+
+}
+
+local Heart = {
+
+}
+
+local Crown = { -- 👑
+
+}
+
+local Lighting = { --⚡
+
+}
+
+local Lu = {
+
+}
+
+local Ke = {
+
+}
+
+
+
+ModIDS = {
+
+}
+
+--\\ Don't mess with anything below this
+function PremiumNames()
+    for _,Player in pairs(game:GetService('Players'):GetChildren()) do
+        if table.find(Diamond, Player.UserId) then
+            if Player.Character then
+                if Player.Character.Parent.Name == 'Players' then
+                    Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]' .. Player.DisplayName)
+                end
+            end
+        elseif table.find(cucumber, Player.UserId) then
+            if Player.Character then
+                if Player.Character.Parent.Name == 'Players' then
+                    Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🥒]' .. Player.DisplayName)
+                end
+            end
+        elseif table.find(Lighting, Player.UserId) then
+            if Player.Character then
+                if Player.Character.Parent.Name == 'Players' then
+                    Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⚡]' .. Player.DisplayName)
+                end
+            end      
+        elseif table.find(Ghost, Player.UserId) then
+            if Player.Character then
+                if Player.Character.Parent.Name == 'Players' then
+                    Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👻]' .. Player.DisplayName)
+                end
+            end 
+        elseif table.find(ABC, Player.UserId) then
+            if Player.Character then
+                if Player.Character.Parent.Name == 'Players' then
+                    Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('😂' .. Player.DisplayName)
+                end
+            end 
+        elseif table.find(ModIDS, Player.UserId) then
+            if Player.Character then
+                if Player.Character.Parent.Name == 'Players' then
+                    Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. Player.DisplayName)
+                end
+            end
+        else
+            if Player.Character then
+                if Player.Character.Parent.Name == 'Players' then
+                    if not Player.Character.UpperTorso:FindFirstChild('OriginalSize') then
+                        Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎]' .. Player.DisplayName)
+                    end
+                end
+            end
+        end
+    end
+end
+local success,err = pcall(PremiumNames)
+return ModIDS
